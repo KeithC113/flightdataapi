1ca0744160d7130b4658346cee3f58641f17d8f6
Keith Campbell <campbell_ka@yahoo.co.uk>
2021-04-08 21:38:41 +0100

    adding in fetch

--- src/App.js
+++ src/App.js
@@ -1,12 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component, useEffect, useState } from 'react';
+import flightDataXML from '../public/flightdata_A.xml';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+axios.get(flightDataXML, {
+  "Content-Type": "application/xml; charset=utf-8"
+})
+.then((response) => {
+  console.log('Your xml file as string', response.data);
+});
+
+class App extends Component{
+  constructor(props){
+    super(props);
+  this.state = { 
+     items:[],
+     isloaded: false
+    }
+  }
+ componentDidMount() {
+    fetch('C:\Users\Keith Campbell\AppData\Local\Temp\Temp1_flighdata_A (1).zip\flighdata_A.xml')
+      .then(res => res.json())
+    .then(json => {
+      this.setState({
+            isloaded: true,
+           items: json 
+        })
+      });
+}
+
+  render() {
+  var { isloaded, items} =this.state;
+
+   if (!isloaded){
+      return <div>Loading...</div>;
+    }
+   else {
+      return (
+      <div className="App">
+       Data has been loaded
+     </div>
+    );
+    }
+  }
 }
 
 export default App;
